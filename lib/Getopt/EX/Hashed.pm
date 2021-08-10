ab527705b91c458e278b9fbd4fcaa19c71b30c97
Kazumasa Utashiro <kaz@utashiro.com>
2021-08-10 17:59:27 +0900

    Move pod text.

--- lib/Getopt/EX/Hashed.pm
+++ lib/Getopt/EX/Hashed.pm
@@ -30,204 +30,6 @@ Version 0.9909
       if ($app->{start}) {
           ...
 
-=head1 DESCRIPTION
-
-B<Getopt::EX::Hashed> is a module to automate a hash object to store
-command line option values.  Major objective of this module is
-integrating initialization and specification into single place.
-Module name shares B<Getopt::EX>, but it works independently from
-other modules included in B<Getopt::EX>, so far.
-
-In the current implementation, using B<Getopt::Long>, or compatible
-module such as B<Getopt::EX::Long> is assumed.  It is configurable,
-but no other module is supported now.
-
-Accessor methods are automatically generated when appropiate parameter
-is given.
-
-=head1 FUNCTION
-
-=head2 B<has>
-
-Declare option parameters in a form of:
-
-    has option_name => ( param => value, ... );
-
-If array reference is given, multiple names can be declared at once.
-
-    has [ 'left', 'right' ] => ( spec => "=i" );
-
-If the name start with plus (C<+>), given parameters are added to
-current value.
-
-    has '+left' => ( default => 1 );
-
-Following parameters are available.
-
-=over 7
-
-=item B<is> => I<ro> | I<rw>
-
-If an B<is> parameter is given, accessor method for the member,
-read-only for I<ro> and read-write for I<rw>, is generated.
-
-=item B<spec> => I<string>
-
-Give option specification.  Option spec and alias names are separated
-by white space, and can show up in any order.
-
-Declaration
-
-    has start => ( spec => "=i s begin" );
-
-will be compiled into string:
-
-    start|s|begin=i
-
-which conform to C<Getopt::Long> definition.  Of course, you can write
-as this:
-
-    has start => ( spec => "s|begin=i" );
-
-If the name and aliases contain underscore (C<_>), another alias name
-is defined with dash (C<->) in place of underscores.  So
-
-    has a_to_z => ( spec => "=s" );
-
-will be compiled into:
-
-    a_to_z|a-to-z:s
-
-If nothing special is necessary, give empty (or white space only)
-string as a value.  Otherwise, it is not considered as an option.
-
-=item B<alias> => I<string>
-
-Additional alias names can be specified by B<alias> parameter too.
-There is no difference with ones in B<spec> parameter.
-
-=item B<default> => I<value>
-
-Set default value.  If no default is given, the member is initialized
-as C<undef>.
-
-=item B<action> => I<coderef>
-
-Parameter B<action> takes code reference which is called to process
-the option.  When called, hash object is passed through C<$_>.
-
-    has [ qw(left right both) ] => spec => '=i';
-    has "+both" => action => sub {
-        $_->{left} = $_->{right} = $_[1];
-    };
-
-You can use this for C<< "<>" >> to catch everything.  In that case,
-spec parameter does not matter and not required.
-
-    has ARGV => default => [];
-    has "<>" => action => sub {
-        push @{$_->{ARGV}}, $_[0];
-    };
-
-In fact, B<default> parameter takes code reference too.  It is stored
-in the hash object and the code works almost same.  But the hash value
-can not be used for option storage.
-
-Because B<action> function intercept the option assignment, it can be
-used to verify the parameter.
-
-    has age =>
-        spec => '=i',
-        action => sub {
-            my($name, $i) = @_;
-            (0 <= $i and $i <= 150) or
-                die "$name: have to be in 0 to 150 range.\n";
-            $_->{$name} = $i;
-        };
-
-=back
-
-=head1 METHOD
-
-=over 7
-
-=item B<new>
-
-Class method to get initialized hash object.
-
-=item B<configure>
-
-There should be some configurable variables, but not fixed yet.
-
-=item B<getopt>
-
-Call C<GetOptions> function defined in caller's context with
-appropriate parameters.
-
-    $obj->getopt
-
-is just a shortcut for:
-
-    GetOptions($obj->optspec)
-
-=item B<optspec>
-
-Return option specification list which can be given to C<GetOptions>
-function.  GetOptions has a capability of storing values in a hash, by
-giving the hash reference as a first argument, but it is not expected.
-
-=item B<use_keys>
-
-Because hash keys are protected by C<Hash::Util::lock_keys>, accessing
-non-existing member causes an error.  Use this function to declare new
-member key before use.
-
-    $obj->use_keys( qw(foo bar) );
-
-If you want to access arbitrary keys, unlock the object.
-
-    use Hash::Util 'unlock_keys';
-    unlock_keys %{$obj};
-
-=item B<reset>
-
-Reset the class to original state.  Because the hash object keeps all
-information, this does not effect to the existing object.  It returns
-the object itself, so you can reset the class after creating a object
-like this:
-
-    my $obj = Getopt::EX::Hashed->new->reset;
-
-This is almost equivalent to the next code:
-
-    my $obj = Getopt::EX::Hashed->new;
-    Getopt::EX::Hashed->reset;
-
-=back
-
-=head1 SEE ALSO
-
-L<Getopt::Long>
-
-L<Getopt::EX>, L<Getopt::EX::Long>
-
-=head1 AUTHOR
-
-Kazumasa Utashiro
-
-=head1 COPYRIGHT
-
-The following copyright notice applies to all the files provided in
-this distribution, including binary files, unless explicitly noted
-otherwise.
-
-Copyright 2021 Kazumasa Utashiro
-
-=head1 LICENSE
-
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself.
-
 =cut
 
 use v5.14;
@@ -428,3 +230,205 @@ sub use_keys {
 }
 
 1;
+
+__END__
+
+=head1 DESCRIPTION
+
+B<Getopt::EX::Hashed> is a module to automate a hash object to store
+command line option values.  Major objective of this module is
+integrating initialization and specification into single place.
+Module name shares B<Getopt::EX>, but it works independently from
+other modules included in B<Getopt::EX>, so far.
+
+In the current implementation, using B<Getopt::Long>, or compatible
+module such as B<Getopt::EX::Long> is assumed.  It is configurable,
+but no other module is supported now.
+
+Accessor methods are automatically generated when appropiate parameter
+is given.
+
+=head1 FUNCTION
+
+=head2 B<has>
+
+Declare option parameters in a form of:
+
+    has option_name => ( param => value, ... );
+
+If array reference is given, multiple names can be declared at once.
+
+    has [ 'left', 'right' ] => ( spec => "=i" );
+
+If the name start with plus (C<+>), given parameters are added to
+current value.
+
+    has '+left' => ( default => 1 );
+
+Following parameters are available.
+
+=over 7
+
+=item B<is> => I<ro> | I<rw>
+
+If an B<is> parameter is given, accessor method for the member,
+read-only for I<ro> and read-write for I<rw>, is generated.
+
+=item B<spec> => I<string>
+
+Give option specification.  Option spec and alias names are separated
+by white space, and can show up in any order.
+
+Declaration
+
+    has start => ( spec => "=i s begin" );
+
+will be compiled into string:
+
+    start|s|begin=i
+
+which conform to C<Getopt::Long> definition.  Of course, you can write
+as this:
+
+    has start => ( spec => "s|begin=i" );
+
+If the name and aliases contain underscore (C<_>), another alias name
+is defined with dash (C<->) in place of underscores.  So
+
+    has a_to_z => ( spec => "=s" );
+
+will be compiled into:
+
+    a_to_z|a-to-z:s
+
+If nothing special is necessary, give empty (or white space only)
+string as a value.  Otherwise, it is not considered as an option.
+
+=item B<alias> => I<string>
+
+Additional alias names can be specified by B<alias> parameter too.
+There is no difference with ones in B<spec> parameter.
+
+=item B<default> => I<value>
+
+Set default value.  If no default is given, the member is initialized
+as C<undef>.
+
+=item B<action> => I<coderef>
+
+Parameter B<action> takes code reference which is called to process
+the option.  When called, hash object is passed through C<$_>.
+
+    has [ qw(left right both) ] => spec => '=i';
+    has "+both" => action => sub {
+        $_->{left} = $_->{right} = $_[1];
+    };
+
+You can use this for C<< "<>" >> to catch everything.  In that case,
+spec parameter does not matter and not required.
+
+    has ARGV => default => [];
+    has "<>" => action => sub {
+        push @{$_->{ARGV}}, $_[0];
+    };
+
+In fact, B<default> parameter takes code reference too.  It is stored
+in the hash object and the code works almost same.  But the hash value
+can not be used for option storage.
+
+Because B<action> function intercept the option assignment, it can be
+used to verify the parameter.
+
+    has age =>
+        spec => '=i',
+        action => sub {
+            my($name, $i) = @_;
+            (0 <= $i and $i <= 150) or
+                die "$name: have to be in 0 to 150 range.\n";
+            $_->{$name} = $i;
+        };
+
+=back
+
+=head1 METHOD
+
+=over 7
+
+=item B<new>
+
+Class method to get initialized hash object.
+
+=item B<configure>
+
+There should be some configurable variables, but not fixed yet.
+
+=item B<getopt>
+
+Call C<GetOptions> function defined in caller's context with
+appropriate parameters.
+
+    $obj->getopt
+
+is just a shortcut for:
+
+    GetOptions($obj->optspec)
+
+=item B<optspec>
+
+Return option specification list which can be given to C<GetOptions>
+function.  GetOptions has a capability of storing values in a hash, by
+giving the hash reference as a first argument, but it is not expected.
+
+=item B<use_keys>
+
+Because hash keys are protected by C<Hash::Util::lock_keys>, accessing
+non-existing member causes an error.  Use this function to declare new
+member key before use.
+
+    $obj->use_keys( qw(foo bar) );
+
+If you want to access arbitrary keys, unlock the object.
+
+    use Hash::Util 'unlock_keys';
+    unlock_keys %{$obj};
+
+=item B<reset>
+
+Reset the class to original state.  Because the hash object keeps all
+information, this does not effect to the existing object.  It returns
+the object itself, so you can reset the class after creating a object
+like this:
+
+    my $obj = Getopt::EX::Hashed->new->reset;
+
+This is almost equivalent to the next code:
+
+    my $obj = Getopt::EX::Hashed->new;
+    Getopt::EX::Hashed->reset;
+
+=back
+
+=head1 SEE ALSO
+
+L<Getopt::Long>
+
+L<Getopt::EX>, L<Getopt::EX::Long>
+
+=head1 AUTHOR
+
+Kazumasa Utashiro
+
+=head1 COPYRIGHT
+
+The following copyright notice applies to all the files provided in
+this distribution, including binary files, unless explicitly noted
+otherwise.
+
+Copyright 2021 Kazumasa Utashiro
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
