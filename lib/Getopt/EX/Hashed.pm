8dae122aa5576c7219a2b5b58eedbb9c84547632
Kazumasa Utashiro <kaz@utashiro.com>
2021-09-17 15:52:46 +0900

    Checking in changes prior to tagging of version 0.9919.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index e36a185..f499f9a 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,10 @@ Revision history for Perl extension Getopt-EX-Hashed
     
     {{$NEXT}}
     
    +0.9919 2021-09-17T06:52:33Z
    +
    +    - Define accessor method only once.
    +
     0.9918 2021-09-12T03:01:33Z
     
         - Unlock keys of config hash just in case.

--- lib/Getopt/EX/Hashed.pm
+++ lib/Getopt/EX/Hashed.pm
@@ -1,6 +1,6 @@
 package Getopt::EX::Hashed;
 
-our $VERSION = '0.9918';
+our $VERSION = '0.9919';
 
 =head1 NAME
 
@@ -8,7 +8,7 @@ Getopt::EX::Hashed - Hash store object automation
 
 =head1 VERSION
 
-Version 0.9918
+Version 0.9919
 
 =head1 SYNOPSIS
 
@@ -150,7 +150,8 @@ sub new {
 	if (my $is = $param{is}) {
 	    no strict 'refs';
 	    my $access = $config->{ACCESSOR_PREFIX} . $name;
-	    *{"$class\::$access"} = _accessor($is, $name);
+	    *{"$class\::$access"} = _accessor($is, $name)
+		unless ${"$class\::"}{$access};
 	}
 	$obj->{$name} = $param{default};
     }
