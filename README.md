57b08c3c58646c3d9935915aba199dc8cb1f6f05
Kazumasa Utashiro <kaz@utashiro.com>
2022-08-23 10:05:51 +0900

    Checking in changes prior to tagging of version 1.0501.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 46c413a..c87992b 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,10 @@ Revision history for Perl extension Getopt-EX-Hashed
     
     {{$NEXT}}
     
    +1.0501 2022-08-23T01:05:14Z
    +
    +    - update document structure
    +
     1.05 2022-08-22T07:50:55Z
     
         - make assignable accessor as a default action (is => 'rw')

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Getopt::EX::Hashed - Hash store object automation for Getopt::Long
 
 # VERSION
 
-Version 1.05
+Version 1.0501
 
 # SYNOPSIS
 
--- lib/Getopt/EX/Hashed.pm
+++ lib/Getopt/EX/Hashed.pm
@@ -1,6 +1,6 @@
 package Getopt::EX::Hashed;
 
-our $VERSION = '1.05';
+our $VERSION = '1.0501';
 
 =head1 NAME
 
@@ -8,7 +8,7 @@ Getopt::EX::Hashed - Hash store object automation for Getopt::Long
 
 =head1 VERSION
 
-Version 1.05
+Version 1.0501
 
 =head1 SYNOPSIS
 
